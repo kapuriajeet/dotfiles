131cf90a06a918a36d096afae6cf6ae7b0f60c3b
kapuriajeet <jeetkapuria@gmail.com>
2025-09-01 16:49:36 +0530

    added oil and snacks

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,24 +1,25 @@
 return {
-  "nvim-treesitter/nvim-treesitter", 
-  branch = 'master', 
-  lazy = false, 
+  "nvim-treesitter/nvim-treesitter",
+  branch = 'master',
+  lazy = false,
   build = ":TSUpdate",
   config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        auto_install = false,
-        ensure_installed = {
-          "bash",
-          "html",
-          "css",
-          "scss",
-          "javascript",
-          "typescript",
-          "json",
-          "lua",
-        },
-        highlight = { enable = true },
-        indent = { enable = false },
-      })
-    end
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      auto_install = false,
+      ensure_installed = {
+        "bash",
+        "html",
+        "css",
+        "scss",
+        "javascript",
+        "typescript",
+        "json",
+        "lua",
+        'python'
+      },
+      highlight = { enable = true },
+      indent = { enable = false },
+    })
+  end
 }
