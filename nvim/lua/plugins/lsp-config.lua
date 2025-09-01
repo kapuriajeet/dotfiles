be0f86feb736055e9f579c3c6a7aa9f60db38f9d
kapuriajeet <jeetkapuria@gmail.com>
2025-09-01 16:02:08 +0530

    lsp

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,5 +1,4 @@
 return {
-  -- Mason core
   {
     "mason-org/mason.nvim",
     lazy = false,
@@ -7,86 +6,40 @@ return {
       require("mason").setup()
     end,
   },
-
-  -- Mason LSPConfig (just for installation)
   {
     "mason-org/mason-lspconfig.nvim",
-    dependencies = {
-      "mason-org/mason.nvim",
-      "neovim/nvim-lspconfig",
+    lazy = false,
+    opts = {
+      auto_install = true,
     },
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {}, -- leave empty, install manually when needed
-      })
-    end,
   },
-
-  -- LSP Configurations
   {
     "neovim/nvim-lspconfig",
-    dependencies = { "hrsh7th/cmp-nvim-lsp" },
+    lazy = false,
     config = function()
+      local cmp_nvim_lsp = require("cmp_nvim_lsp")
+      local capabilities = vim.tbl_deep_extend(
+        "force",
+        {},
+        vim.lsp.protocol.make_client_capabilities(),
+        cmp_nvim_lsp.default_capabilities()
+      )
+
       local lspconfig = require("lspconfig")
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      -- Common function to attach keymaps
-      local on_attach = function(_, bufnr)
-        local opts = { buffer = bufnr, noremap = true, silent = true }
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-        vim.keymap.set("n", "<leader>f", function()
-          vim.lsp.buf.format({ async = true })
-        end, opts)
-      end
-
-      -- Example: setup for Lua
+      lspconfig.tailwindcss.setup({
+        capabilities = capabilities
+      })
       lspconfig.lua_ls.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-        settings = {
-          Lua = {
-            diagnostics = { globals = { "vim" } },
-            workspace = { checkThirdParty = false },
-          },
-        },
+        capabilities = capabilities
       })
 
-  -- Python
-  lspconfig.pyright.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-  })
-
-  -- TypeScript / JavaScript
-  lspconfig.ts_ls.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-  })
-
-  -- HTML
-  lspconfig.html.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-  })
-  lspconfig.emmet_ls.setup({
-    -- on_attach = on_attach,
-    capabilities = capabilities,
-    filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue" },
-    init_options = {
-      html = {
-        options = {
-          -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
-          ["bem.enabled"] = true,
-        },
-      },
-    }
-})
-
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {})
+      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
     end,
   },
 }
